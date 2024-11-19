075c8bbda1b72ad01e1e837b0b4a7975d4b3ab88
lauschof <schofieldlaura@outlook.com>
2024-11-19 20:21:00 +0800

    adding projects

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,18 @@
+import useProjects from "../hooks/use-projects";
+import ProjectCard from "../components/ProjectCard";
+import "../HomePage.css";
+
+
 function HomePage() {
-    return <h1>This is the home page.</h1>;
-  }
+  const { projects } = useProjects();
+
+  return (
+    <div id="project-list">
+      {projects.map((projectData, key) => {
+        return <ProjectCard key={key} projectData={projectData} />;
+      })}
+    </div>
+  );
+}
   
   export default HomePage;
