b5ed6dba4e30f8d1e15458ec426a3523ce8ebeda
lauschof <schofieldlaura@outlook.com>
2024-11-26 20:18:33 +0800

    adding APIs

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import HomePage from "./pages/HomePage.jsx";
 import ProjectPage from "./pages/ProjectPage.jsx";
-
+import LoginPage from "./pages/LoginPage.jsx";
 import NavBar from "./components/NavBar.jsx";
 
 const router = createBrowserRouter([
@@ -13,6 +13,7 @@ const router = createBrowserRouter([
       element: <NavBar />,
       children: [
           { path: "/", element: <HomePage /> },
+          { path: "/login", element: <LoginPage /> },
           { path: "/project:id", element: <ProjectPage /> },
       ],
   },
